d8421effdaaeb53dc10cc9fd242883b29c767545
georgeding <dingchangzhao@hotmail.com>
2023-07-16 00:43:09 +0800

    to add more to the category and match the schema

--- src/pages/EateryCategoryPage/EateryCategoryPage.jsx
+++ src/pages/EateryCategoryPage/EateryCategoryPage.jsx
@@ -2,24 +2,67 @@ import { useState } from "react";
 
 export default function EateryCategoryPage() {
   const [categoryName, setCategoryName] = useState([]);
-  const [typedValue, setTypedValue] = useState("")
+  const [categoryImage, setCategoryImage] = useState([]);
+  const [categoryDesc, setCategoryDesc] = useState([]);
 
-  const handleChange = (evt) => {
-    setTypedValue(evt.target.value);
+  const [nameTypedValue, setNameTypedValue] = useState("");
+  const [imageTypedValue, setImageTypedValue] = useState("");
+  const [descTypedValue, setDescTypedValue] = useState("");
+
+  const handleNameChange = (evt) => {
+    setNameTypedValue(evt.target.value);
   };
+
+  const handleImageChange = (evt) => {
+    setImageTypedValue(evt.target.value);
+  };
+
+  const handleDescChange = (evt) => {
+    setDescTypedValue(evt.target.value);
+  };
+
   const handleSubmit = (evt) => {
-    setCategoryName([...categoryName, typedValue]);
-    setTypedValue("")
-    console.log(JSON.stringify(categoryName));
+    evt.preventDefault();
+
+    setCategoryName([...categoryName, nameTypedValue]);
+    setCategoryImage([...categoryImage, imageTypedValue]);
+    setCategoryDesc([...categoryDesc, descTypedValue]);
+
+    setNameTypedValue("");
+    setImageTypedValue("");
+    setDescTypedValue("");
   };
   return (
     <>
       <h1>To add eatery categories</h1>
-      <input type="text" value={typedValue} onChange={handleChange}></input>
-      <button onClick={handleSubmit}>Add an eatery</button>
-      {categoryName.map((category, index) => (
-        <p key={index}>{category}</p>
-      ))}
+      <form>
+        Name:
+        <input type="text" value={nameTypedValue} onChange={handleNameChange} />
+        <br />
+        Image URL:
+        <input
+          type="text"
+          value={imageTypedValue}
+          onChange={handleImageChange}
+        />
+        <br />
+        Description:
+        <input type="text" value={descTypedValue} onChange={handleDescChange} />
+        <br />
+        <button onClick={handleSubmit}>Add an eatery</button>
+      </form>
+
+      <div>
+        {categoryName.map((name, index) => (
+          <p key={index}>{name}</p>
+        ))}
+        {categoryImage.map((image, index) => (
+          <p key={index}>{image}</p>
+        ))}
+        {categoryDesc.map((desc, index) => (
+          <p key={index}>{desc}</p>
+        ))}
+      </div>
     </>
   );
 }
