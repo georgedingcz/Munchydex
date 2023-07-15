39b2bc7a41ea3e399c688de83b4ecd9638761607
georgeding <dingchangzhao@hotmail.com>
2023-07-16 00:02:02 +0800

    frontend test for visualisation of adding categories

--- src/pages/EateryCategoryPage/EateryCategoryPage.jsx
+++ src/pages/EateryCategoryPage/EateryCategoryPage.jsx
@@ -1,9 +1,25 @@
-export default function EateryCategoryPage () {
-    return (
-        <>
-        <h1>
-            To add eatery categories
-        </h1>
-        </>
-    )
+import { useState } from "react";
+
+export default function EateryCategoryPage() {
+  const [categoryName, setCategoryName] = useState([]);
+  const [typedValue, setTypedValue] = useState("")
+
+  const handleChange = (evt) => {
+    setTypedValue(evt.target.value);
+  };
+  const handleSubmit = (evt) => {
+    setCategoryName([...categoryName, typedValue]);
+    setTypedValue("")
+    console.log(JSON.stringify(categoryName));
+  };
+  return (
+    <>
+      <h1>To add eatery categories</h1>
+      <input type="text" value={typedValue} onChange={handleChange}></input>
+      <button onClick={handleSubmit}>Add an eatery</button>
+      {categoryName.map((category, index) => (
+        <p key={index}>{category}</p>
+      ))}
+    </>
+  );
 }
