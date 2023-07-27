0cd42b7e998798e1a593e050ebb0cc155c613fc9
georgeding <dingchangzhao@hotmail.com>
2023-07-28 00:28:36 +0800

    add existing eateries to indv category page

--- src/pages/EateryCategoryPage/IndvCatPage.jsx
+++ src/pages/EateryCategoryPage/IndvCatPage.jsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
-import { Card, Container } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function IndvCat() {
+export default function IndvCat({
+  setNewMegaState,
+  newMegaState,
+  setForEateryFetch,
+  forEateryFetch,
+  existingEateries,
+}) {
   const [oneCat, setOneCat] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleOneEatPage = (id) => {
+    navigate(`/eatery/` + id);
+    console.log(id);
+  };
 
   const { id } = useParams();
   useEffect(() => {
@@ -18,9 +31,16 @@ export default function IndvCat() {
         if (response.ok) {
           const data = await response.json();
           setOneCat(data);
+
+          //
+          await setNewMegaState({
+            ...newMegaState,
+            categoryID: id,
+          });
+          setForEateryFetch(!forEateryFetch);
+          //
         } else {
-          console.log("Failed to get the chosen category"
-          )
+          console.log("Failed to get the chosen category");
         }
       } catch (err) {
         console.log(err);
@@ -38,6 +58,35 @@ export default function IndvCat() {
           <Card.Text>{oneCat?.briefDesc}</Card.Text>
         </Card.Body>
       </Card>
+      <Container>
+        {existingEateries.length === 0 ? (
+          <>No eateries for this category</>
+        ) : (
+          <Row xs={1} md={2} lg={3}>
+            {existingEateries.map((existingEatery, index) => (
+              <Col key={index}>
+                <Card
+                  key={index}
+                  style={{ width: "18rem" }}
+                  onClick={() => handleOneEatPage(existingEatery?._id)}
+                >
+                  <Card.Img
+                    src={existingEatery?.image}
+                    alt="eatery"
+                    width="50"
+                    height="200"
+                    variant="top"
+                  />
+                  <Card.Body>
+                    <Card.Title>{existingEatery?.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+                <br />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
     </Container>
   );
 }
