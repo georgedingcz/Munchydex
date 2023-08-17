28a235bdf03fd9cc9ea387fd6c89db4139300f17
georgeding <dingchangzhao@hotmail.com>
2023-08-17 15:10:14 +0800

    update eatery forms for usecontext

--- src/components/Eatery/EatFormCtrl/EatImageFormCtrl.jsx
+++ src/components/Eatery/EatFormCtrl/EatImageFormCtrl.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import { Form } from "react-bootstrap";
+import { MunchyContext } from "../../../pages/App/App";
 
-export default function EatImageFormCtrl({ newMegaState, handleChange }) {
+export default function EatImageFormCtrl() {
+  const context = useContext(MunchyContext);
+
   return (
     <Form.Group className="mb-3" controlId="eatImage">
       <Form.Label className="form-label">Image</Form.Label>
       <Form.Control
         type="text"
         name="eateryImage"
-        value={newMegaState.eateryImage}
-        onChange={handleChange}
+        value={context.newMegaState.eateryImage}
+        onChange={context.handleChange}
       />
     </Form.Group>
   );
--- src/components/Eatery/EatFormCtrl/EatNameFormCtrl.jsx
+++ src/components/Eatery/EatFormCtrl/EatNameFormCtrl.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import { Form } from "react-bootstrap";
+import { MunchyContext } from "../../../pages/App/App";
 
-export default function EatNameFormCtrl({ newMegaState, handleChange }) {
+export default function EatNameFormCtrl() {
+  const context = useContext(MunchyContext);
+
   return (
     <Form.Group className="mb-3" controlId="eatName">
       <Form.Label className="form-label">Name</Form.Label>
       <Form.Control
         type="text"
         name="eateryName"
-        value={newMegaState.eateryName}
-        onChange={handleChange}
+        value={context.newMegaState.eateryName}
+        onChange={context.handleChange}
       />
       <Form.Text id="eatName" muted>
         (Required)
